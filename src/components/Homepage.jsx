0ce97de2fca08b086f40cbd4877d5ea6be934856
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2023-12-22 21:50:49 +0100

    Provide Crypto stats with real numbers

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -7,8 +7,10 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Title = Typography;
 function Homepage() {
   const { data, isFetching } = useGetCryptosQuery();
-
+const globalStats = data?.data?.stats;
   console.log(data);
+
+  if(isFetching) return 'Loading';
 
   return (
     <>
@@ -17,19 +19,19 @@ function Homepage() {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
+          <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value="5" />
+          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value="5" />
+          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
+          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
         </Col>
       </Row>
     </>
