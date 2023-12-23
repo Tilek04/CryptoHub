8417b891aef158b80103da8a790ef82298756c8d
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2023-12-23 18:33:05 +0100

    Get all data for cryptocurrentcies and done this page as well

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useState } from "react";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurriences = () => {
+const Cryptocurrencies = () => {
+  const { data: cryptosList, isFetching } = useGetCryptosQuery();
+  const cryptos = cryptosList?.data?.coins;
+
+  if (!cryptos) {
+    return null;
+  }
+
   return (
-    <div>Cryptocurriences</div>
-  )
-}
+    <>
+      <Row gutters={[32, 32]} className="crypto-container">
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={
+                  <img
+                    className="crypto-img"
+                    src={currency.iconUrl}
+                    alt={currency.name}
+                    style={{width: '40px'}}
+                    hoverable
+                  />
+                }>
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default Cryptocurriences
+export default Cryptocurrencies;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,14 +3,16 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { useGetCryptosQuery } from "../services/cryptoApi";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
 
-const Title = Typography;
+const { Title } = Typography;
 function Homepage() {
   const { data, isFetching } = useGetCryptosQuery();
-const globalStats = data?.data?.stats;
+  const globalStats = data?.data?.stats;
   console.log(data);
 
-  if(isFetching) return 'Loading';
+  if (isFetching) return "Loading";
 
   return (
     <>
@@ -22,18 +24,50 @@ const globalStats = data?.data?.stats;
           <Statistic title="Total Cryptocurrencies" value={globalStats.total} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} />
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats.totalExchanges)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} />
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats.totalMarketCap)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} />
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats.total24hVolume)}
+          />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)} />
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+          />
         </Col>
       </Row>
+
+      <div className="home-container">
+        <Title level={1} className="home-title">
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+
+      <div className="home-container">
+        <Title level={1} className="home-title">
+          Latest crypro News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
