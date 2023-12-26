d598a0ae3288baa2b1c184d469402a48a7046980
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2023-12-26 21:08:29 +0100

    Done news page work with api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,7 +4,7 @@ import { Card, Row, Col, Input, Typography } from "antd";
 const News = ({ simplifyed }) => {
   const { data: cryptoNews } = useGetNewsQuery({
     newsCategory: "Cryptocurrency",
-    count: simplifyed ? 10 : 100
+    count: simplifyed ? 10 : 100,
   });
 
   const { Title } = Typography;
@@ -16,11 +16,17 @@ const News = ({ simplifyed }) => {
           <Col xs={24} sm={12} lg={8} key={news.id}>
             <Card hoverable className="newsBlock">
               <a href={news.url} target="_blank">
-                <div className="news-container">
-                  <Title className="newsTitle" level={4}>
+                <div className="news-container" style={{display: 'flex'}}>
+                  <Title className="newsTitle" level={5}>
                     {news.title}
                   </Title>
+                  <img src={news.thumbnail} style={{ width: "50%", height: '50%' }} />
+                
                 </div>
+             
+                <p>{news.description}</p>
+                <p style={{color: 'grey', fontSize: '13px'}}>{news.createdAt}</p>
+            
               </a>
             </Card>
           </Col>
