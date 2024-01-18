cc88615dd4a64b689b13f9ea89a10509ee15df05
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2024-01-18 20:24:04 +0100

    fixed bag with Crypto id

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import HTMLReactParser from "html-react-parser";
+import { useParams } from "react-router-dom";
+import millify from "millify";
+import { Col, Row, Typography, Select } from "antd";
+import {
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  DollarCircleOutlined,
+  ExclamationCircleOutlined,
+  StopOutlined,
+  TrophyOutlined,
+} from "@ant-design/icons";
+
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
-  return (
-    <div>CryptoDetails</div>
-  )
-}
+const { coinId } = useParams();
+console.log(coinId);
 
-export default CryptoDetails
+  return <div>CryptoDetails {coinId}</div>;
+};
+
+export default CryptoDetails;
--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -8,11 +8,11 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Cryptocurrencies = ({ simplifyed }) => {
   const count = simplifyed ? 10 : 100;
   const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
-  // const cryptos = cryptosList?.data?.coins;
   const [cryptos, setCryptos] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
+    console.log(cryptosList)
     const filterData = cryptosList?.data?.coins.filter((coin) =>
       coin.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
     );
@@ -35,9 +35,30 @@ const Cryptocurrencies = ({ simplifyed }) => {
       )}
 
       <Row gutter={[32, 32]} className="crypto-container">
-        {cryptos.map((currency) => (
+      {cryptos.map((currency) => (
+  <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+    <Link to={`/crypto/${currency.uuid}`}>
+    <Card
+                title={`${currency.rank}. ${currency.name} `}
+                extra={
+                  <img
+                    className="crypto-img"
+                    src={currency.iconUrl}
+                    alt={currency.name}
+                    style={{ width: "40px" }}
+                    hoverable
+                  />
+                }>
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}</p>
+              </Card>
+    </Link>
+  </Col>
+))}
+      {/* {cryptosList && cryptosList.data.coins.map((currency) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-            <Link to={`/crypto/${currency.id}`}>
+            <Link to={`/crypto/${currency.id}`} onClick={() => console.log(currency.id)}>
               <Card
                 title={`${currency.rank}. ${currency.name}`}
                 extra={
@@ -55,7 +76,7 @@ const Cryptocurrencies = ({ simplifyed }) => {
               </Card>
             </Link>
           </Col>
-        ))}
+        ))} */}
       </Row>
     </>
   );
